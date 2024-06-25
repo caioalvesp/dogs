7d1725fdd4c7d2649fe24a7dfa9d3d1356cb3b7b
Caio Alves <78694107+caioalvesp@users.noreply.github.com>
2024-06-25 16:41:56 +0000

    estilizacao do menu mobile e criacao do fluxo de post

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { UserContext } from "../../UserContext";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import MinhasFotos from "../../Assets/feed.svg?react";
 import Estatisticas from "../../Assets/estatisticas.svg?react";
 import AdicionarFoto from "../../Assets/adicionar.svg?react";
@@ -20,15 +20,23 @@ const UserHeaderNav = () => {
     navigate("/login");
   }
 
+  const { pathname } = useLocation();
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname])
+
   return (
     <>
       {mobile && (
         <button
           aria-label="Menu"
+          className={`${styles.mobileButton} ${mobileMenu && styles.mobileButtonActive}`}
           onClick={() => setMobileMenu(!mobileMenu)}
         ></button>
       )}
-      <nav className={styles.nav}>
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav} ${mobileMenu && styles.navMobileActive}`}
+      >
         <NavLink to="/conta" end>
           <MinhasFotos />
           {mobile && "Minhas fotos"}
